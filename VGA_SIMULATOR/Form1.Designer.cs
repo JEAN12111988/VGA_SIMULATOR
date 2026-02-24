913ea25c3f8f8dfbbb3c78a6dd55d8c7e520211a
Jean de Souza <jeandesouzaprograming@gmail.com>
2026-02-24 07:39:58 -0300

    first commit

---
+++ VGA_SIMULATOR/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace VGA_SIMULATOR
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.picTELA = new System.Windows.Forms.PictureBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.pBarFrame = new System.Windows.Forms.ProgressBar();
+			this.rdMultiF = new System.Windows.Forms.RadioButton();
+			this.rdUnicoF = new System.Windows.Forms.RadioButton();
+			this.btnCarregar = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCarregaOUT = new System.Windows.Forms.Button();
+			this.txtSIM = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnCarregaPPM = new System.Windows.Forms.Button();
+			this.txtPPM = new System.Windows.Forms.TextBox();
+			this.txtLOG = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.btnLimparLOG = new System.Windows.Forms.Button();
+			this.btnExportLOG = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.picTELA)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// picTELA
+			// 
+			this.picTELA.Location = new System.Drawing.Point(16, 43);
+			this.picTELA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.picTELA.Name = "picTELA";
+			this.picTELA.Size = new System.Drawing.Size(853, 591);
+			this.picTELA.TabIndex = 0;
+			this.picTELA.TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.pBarFrame);
+			this.groupBox1.Controls.Add(this.rdMultiF);
+			this.groupBox1.Controls.Add(this.rdUnicoF);
+			this.groupBox1.Controls.Add(this.btnCarregar);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.btnCarregaOUT);
+			this.groupBox1.Controls.Add(this.txtSIM);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.btnCarregaPPM);
+			this.groupBox1.Controls.Add(this.txtPPM);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox1.Location = new System.Drawing.Point(903, 43);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox1.Size = new System.Drawing.Size(451, 293);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "ARQUIVOS";
+			// 
+			// pBarFrame
+			// 
+			this.pBarFrame.Location = new System.Drawing.Point(8, 240);
+			this.pBarFrame.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.pBarFrame.Name = "pBarFrame";
+			this.pBarFrame.Size = new System.Drawing.Size(276, 28);
+			this.pBarFrame.TabIndex = 9;
+			// 
+			// rdMultiF
+			// 
+			this.rdMultiF.AutoSize = true;
+			this.rdMultiF.Location = new System.Drawing.Point(159, 41);
+			this.rdMultiF.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.rdMultiF.Name = "rdMultiF";
+			this.rdMultiF.Size = new System.Drawing.Size(119, 24);
+			this.rdMultiF.TabIndex = 8;
+			this.rdMultiF.TabStop = true;
+			this.rdMultiF.Text = "Multi Frame";
+			this.rdMultiF.UseVisualStyleBackColor = true;
+			// 
+			// rdUnicoF
+			// 
+			this.rdUnicoF.AutoSize = true;
+			this.rdUnicoF.Location = new System.Drawing.Point(8, 41);
+			this.rdUnicoF.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.rdUnicoF.Name = "rdUnicoF";
+			this.rdUnicoF.Size = new System.Drawing.Size(126, 24);
+			this.rdUnicoF.TabIndex = 7;
+			this.rdUnicoF.TabStop = true;
+			this.rdUnicoF.Text = "Unico Frame";
+			this.rdUnicoF.UseVisualStyleBackColor = true;
+			// 
+			// btnCarregar
+			// 
+			this.btnCarregar.Location = new System.Drawing.Point(292, 226);
+			this.btnCarregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnCarregar.Name = "btnCarregar";
+			this.btnCarregar.Size = new System.Drawing.Size(151, 58);
+			this.btnCarregar.TabIndex = 6;
+			this.btnCarregar.Text = "CARREGAR";
+			this.btnCarregar.UseVisualStyleBackColor = true;
+			this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(8, 150);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(132, 20);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Caminho do SIM";
+			// 
+			// btnCarregaOUT
+			// 
+			this.btnCarregaOUT.Location = new System.Drawing.Point(395, 175);
+			this.btnCarregaOUT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnCarregaOUT.Name = "btnCarregaOUT";
+			this.btnCarregaOUT.Size = new System.Drawing.Size(48, 28);
+			this.btnCarregaOUT.TabIndex = 4;
+			this.btnCarregaOUT.Text = "...";
+			this.btnCarregaOUT.UseVisualStyleBackColor = true;
+			this.btnCarregaOUT.Click += new System.EventHandler(this.btnCarregaOUT_Click);
+			// 
+			// txtSIM
+			// 
+			this.txtSIM.Location = new System.Drawing.Point(8, 175);
+			this.txtSIM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtSIM.Name = "txtSIM";
+			this.txtSIM.Size = new System.Drawing.Size(377, 26);
+			this.txtSIM.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(8, 82);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(146, 20);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Caminho do frame";
+			// 
+			// btnCarregaPPM
+			// 
+			this.btnCarregaPPM.Location = new System.Drawing.Point(395, 107);
+			this.btnCarregaPPM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnCarregaPPM.Name = "btnCarregaPPM";
+			this.btnCarregaPPM.Size = new System.Drawing.Size(48, 28);
+			this.btnCarregaPPM.TabIndex = 1;
+			this.btnCarregaPPM.Text = "...";
+			this.btnCarregaPPM.UseVisualStyleBackColor = true;
+			this.btnCarregaPPM.Click += new System.EventHandler(this.btnCarregaPPM_Click);
+			// 
+			// txtPPM
+			// 
+			this.txtPPM.Location = new System.Drawing.Point(8, 107);
+			this.txtPPM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtPPM.Name = "txtPPM";
+			this.txtPPM.Size = new System.Drawing.Size(377, 26);
+			this.txtPPM.TabIndex = 0;
+			// 
+			// txtLOG
+			// 
+			this.txtLOG.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtLOG.Location = new System.Drawing.Point(903, 375);
+			this.txtLOG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtLOG.Multiline = true;
+			this.txtLOG.Name = "txtLOG";
+			this.txtLOG.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtLOG.Size = new System.Drawing.Size(449, 219);
+			this.txtLOG.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(907, 351);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(45, 20);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "LOG";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// btnLimparLOG
+			// 
+			this.btnLimparLOG.Location = new System.Drawing.Point(903, 603);
+			this.btnLimparLOG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnLimparLOG.Name = "btnLimparLOG";
+			this.btnLimparLOG.Size = new System.Drawing.Size(151, 34);
+			this.btnLimparLOG.TabIndex = 10;
+			this.btnLimparLOG.Text = "LIMPAR LOG";
+			this.btnLimparLOG.UseVisualStyleBackColor = true;
+			this.btnLimparLOG.Click += new System.EventHandler(this.btnLimparLOG_Click);
+			// 
+			// btnExportLOG
+			// 
+			this.btnExportLOG.Location = new System.Drawing.Point(1061, 604);
+			this.btnExportLOG.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.btnExportLOG.Name = "btnExportLOG";
+			this.btnExportLOG.Size = new System.Drawing.Size(151, 34);
+			this.btnExportLOG.TabIndex = 11;
+			this.btnExportLOG.Text = "EXPORTAR LOG";
+			this.btnExportLOG.UseVisualStyleBackColor = true;
+			this.btnExportLOG.Click += new System.EventHandler(this.btnExportLOG_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1369, 654);
+			this.Controls.Add(this.btnExportLOG);
+			this.Controls.Add(this.btnLimparLOG);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtLOG);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.picTELA);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "Form1";
+			this.Text = "VGA SIMULATOR";
+			((System.ComponentModel.ISupportInitialize)(this.picTELA)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox picTELA;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnCarregaPPM;
+		private System.Windows.Forms.TextBox txtPPM;
+		private System.Windows.Forms.Button btnCarregar;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnCarregaOUT;
+		private System.Windows.Forms.TextBox txtSIM;
+		private System.Windows.Forms.TextBox txtLOG;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.RadioButton rdMultiF;
+		private System.Windows.Forms.RadioButton rdUnicoF;
+		private System.Windows.Forms.ProgressBar pBarFrame;
+		private System.Windows.Forms.Button btnLimparLOG;
+		private System.Windows.Forms.Button btnExportLOG;
+	}
+}
+
---
+++ VGA_SIMULATOR/Form1.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VGA_SIMULATOR
+{
+	public partial class Form1 : Form
+	{
+		string caminhoPPM;
+		string caminhoOUT;
+		private bool rodando = false;
+		int frameIndex = 0;
+		int totalFrames = 0;
+		private string pastaFrames;
+		private bool simulacaoCarregada = false;
+		private Process processoSimulacao;
+
+		public Form1()
+		{
+			InitializeComponent();
+			this.DoubleBuffered = true;
+
+			rdUnicoF.Checked = true;
+			pBarFrame.Visible = false;
+			pBarFrame.Minimum = 0;
+			WriteLineThreadLOG(txtLOG, "Software Inicializado...");
+		}
+
+		private void btnCarregaPPM_Click(object sender, EventArgs e)
+		{
+			if (rdMultiF.Checked)
+			{
+				using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+				{
+					folderDialog.Description = "Selecione a pasta onde estão os frames";
+
+					if (!string.IsNullOrEmpty(Properties.Settings.Default.UltimaPastaFrames))
+					{
+						folderDialog.SelectedPath = Properties.Settings.Default.UltimaPastaFrames;
+					}
+
+					if (folderDialog.ShowDialog() == DialogResult.OK)
+					{
+						pastaFrames = folderDialog.SelectedPath;
+						txtPPM.Text = pastaFrames;
+
+						try
+						{
+							totalFrames = Directory.GetFiles(pastaFrames, "frame_*.ppm").Length;
+							if (totalFrames == 0)
+							{
+								WriteLineThreadLOG(txtLOG, "Nenhum frame encontrado na pasta.");
+								ShowMessageThread("Nenhum frame encontrado na pasta", "Erro ao carregar os frames", MessageBoxIcon.Error);
+								return;
+							}
+								
+						}
+						catch(Exception ex)
+						{
+							WriteLineThreadLOG(txtLOG, "Erro ao acessar pasta de frames: " + ex.Message);
+						}
+						
+						frameIndex = 0;
+
+						Properties.Settings.Default.UltimaPastaFrames = pastaFrames;
+						Properties.Settings.Default.Save();
+					}
+				}
+			}
+			else if (rdUnicoF.Checked)
+			{
+				OpenFileDialog openFileDialog = new OpenFileDialog();
+				openFileDialog.Title = "Selecionar Frame Único";
+				openFileDialog.Filter = "Arquivo PPM (*.ppm)|*.ppm";
+				openFileDialog.Multiselect = false;
+
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					caminhoPPM = openFileDialog.FileName;
+					txtPPM.Text = caminhoPPM;
+					totalFrames = 1;  // importante
+				}
+			}
+		}
+
+		private void btnCarregaOUT_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Title = "Selecionar Arquivo de SIMULACAO";
+			openFileDialog.Filter = "Arquivos out (*.out)|*.out";
+			openFileDialog.Multiselect = false; // permite selecionar apenas 1 arquivo
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				caminhoOUT = openFileDialog.FileName;
+				txtSIM.Text = caminhoOUT;
+			}
+		}
+
+		private async Task RunSimulationAsync()
+		{
+			await Task.Run(() =>
+			{
+				Process p = new Process();
+				p.StartInfo.FileName = "vvp";
+				p.StartInfo.Arguments = caminhoOUT;
+				p.StartInfo.UseShellExecute = false;
+				p.StartInfo.CreateNoWindow = true;
+
+				p.Start();
+				p.WaitForExit();
+			});
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			try
+			{
+				if (rdUnicoF.Checked)
+				{
+					timer1.Stop();
+					LoadFrame();
+					return;
+				}
+
+				// modo múltiplos frames
+				if (totalFrames == 0)
+					return;
+
+				string file = Path.Combine(pastaFrames, $"frame_{frameIndex}.ppm");
+
+				caminhoPPM = file;
+				LoadFrame();
+
+				frameIndex++;
+
+				if (frameIndex >= totalFrames)
+					frameIndex = 0;  // loop perfeito
+			}
+			catch (Exception ex)
+			{
+				timer1.Stop();
+				WriteLineThreadLOG(txtLOG, "Erro no timer: " + ex.Message);
+			}
+		}
+
+		private void LoadFrame()
+		{
+			try
+			{
+				if (!File.Exists(caminhoPPM))
+				{
+					WriteLineThreadLOG(txtLOG, "Arquivo PPM não encontrado.");
+					return;
+				}
+
+				using (FileStream fs = new FileStream(caminhoPPM, FileMode.Open))
+				using (BinaryReader br = new BinaryReader(fs))
+				{
+					string magic = ReadLine(br);
+					string size = ReadLine(br);
+					string max = ReadLine(br);
+
+					string[] dims = size.Split(' ');
+					int width = int.Parse(dims[0]);
+					int height = int.Parse(dims[1]);
+
+					Bitmap bmp = new Bitmap(width, height,
+						System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+					var rect = new Rectangle(0, 0, width, height);
+					var bmpData = bmp.LockBits(rect,
+						System.Drawing.Imaging.ImageLockMode.WriteOnly,
+						bmp.PixelFormat);
+
+					IntPtr ptr = bmpData.Scan0;
+					int bytes = Math.Abs(bmpData.Stride) * height;
+					byte[] rgbValues = br.ReadBytes(width * height * 3);
+
+					System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, rgbValues.Length);
+
+					bmp.UnlockBits(bmpData);
+
+					picTELA.Image = bmp;
+				}
+			}
+			catch (Exception ex)
+			{
+				WriteLineThreadLOG(txtLOG, "Erro ao carregar frame: " + ex.Message);
+			}
+		}
+
+
+		private string ReadLine(BinaryReader br)
+		{
+			List<byte> bytes = new List<byte>();
+			byte b;
+
+			while (true)
+			{
+				b = br.ReadByte();
+				if (b == 10) break; // '\n'
+				if (b != 13) bytes.Add(b); // ignora '\r'
+			}
+
+			return System.Text.Encoding.ASCII.GetString(bytes.ToArray()).Trim();
+		}
+
+		private async void btnCarregar_Click(object sender, EventArgs e)
+		{
+			if (simulacaoCarregada)
+			{
+				ResetarSimulacao();
+				return;
+			}
+
+			if (string.IsNullOrEmpty(caminhoOUT))
+			{
+				MessageBox.Show("Selecione o arquivo .out primeiro");
+				return;
+			}
+
+			frameIndex = 0;
+
+			pBarFrame.Visible = true;
+			pBarFrame.Style = ProgressBarStyle.Blocks;
+			pBarFrame.Minimum = 0;
+			pBarFrame.Maximum = 100;
+			pBarFrame.Value = 0;
+
+			processoSimulacao = new Process();
+			processoSimulacao.StartInfo.FileName = "vvp";
+			processoSimulacao.StartInfo.Arguments = caminhoOUT;
+			processoSimulacao.StartInfo.UseShellExecute = false;
+			processoSimulacao.StartInfo.CreateNoWindow = true;
+			processoSimulacao.EnableRaisingEvents = false; // NÃO vamos usar evento Exited
+
+			try
+			{
+				processoSimulacao.Start();
+				WriteLineThreadLOG(txtLOG, "Carregando Frame, Aguarde..");
+
+				int progress = 0;
+
+				var progressTask = Task.Run(async () =>
+				{
+					while (!processoSimulacao.HasExited)
+					{
+						if (progress < 95)
+							progress++;
+
+						Invoke(new Action(() =>
+						{
+							pBarFrame.Value = progress;
+						}));
+
+						await Task.Delay(30);
+					}
+				});
+
+				await Task.Run(() => processoSimulacao.WaitForExit());
+				await progressTask;
+
+				pBarFrame.Value = 100;
+				await Task.Delay(200);
+			}
+			catch (Exception ex)
+			{
+				WriteLineThreadLOG(txtLOG, "Erro ao executar simulação: " + ex.Message);
+				pBarFrame.Visible = false;
+				return;
+			}
+
+			pBarFrame.Visible = false;
+
+			if (rdMultiF.Checked)
+			{
+				timer1.Interval = 60;
+				timer1.Start();
+			}
+			else
+			{
+				LoadFrame();
+			}
+
+			simulacaoCarregada = true;
+			btnCarregar.Text = "RECARREGAR";
+
+			WriteLineThreadLOG(txtLOG, "Frame carregado!");
+		}
+
+		// funções auxiliares
+		public void WriteLineThreadLOG(TextBox txtLog, string msg)
+		{
+			string linha = $"[{DateTime.Now:dd/MM/yy HH:mm:ss}] {msg}{Environment.NewLine}";
+
+			if (txtLog.InvokeRequired)
+				txtLog.Invoke(new Action(() => txtLog.AppendText(linha)));
+			else
+				txtLog.AppendText(linha);
+		}
+
+		public void ShowMessageThread(string msg,
+							  string titulo = "Aviso",
+							  MessageBoxIcon icon = MessageBoxIcon.Information)
+		{
+			if (this.InvokeRequired)
+			{
+				this.Invoke(new Action(() =>
+				{
+					MessageBox.Show(this, msg, titulo,
+						MessageBoxButtons.OK, icon);
+				}));
+			}
+			else
+			{
+				MessageBox.Show(this, msg, titulo,
+					MessageBoxButtons.OK, icon);
+			}
+		}
+
+
+		private void btnLimparLOG_Click(object sender, EventArgs e)
+		{
+			txtLOG.Clear();
+		}
+
+		private void btnExportLOG_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(txtLOG.Text))
+			{
+				MessageBox.Show("O log está vazio.", "Aviso",
+					MessageBoxButtons.OK, MessageBoxIcon.Information);
+				return;
+			}
+
+			using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+			{
+				saveFileDialog.Title = "Salvar Log";
+				saveFileDialog.Filter = "Arquivo de Texto (*.txt)|*.txt";
+				saveFileDialog.DefaultExt = "txt";
+				saveFileDialog.FileName = $"LOG_{DateTime.Now:ddMMyy_HHmmss}.txt";
+
+				if (saveFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					try
+					{
+						File.WriteAllText(saveFileDialog.FileName, txtLOG.Text);
+
+						MessageBox.Show("Log exportado com sucesso!",
+							"Sucesso",
+							MessageBoxButtons.OK,
+							MessageBoxIcon.Information);
+					}
+					catch (Exception ex)
+					{
+						MessageBox.Show("Erro ao salvar arquivo:\n" + ex.Message,
+							"Erro",
+							MessageBoxButtons.OK,
+							MessageBoxIcon.Error);
+					}
+				}
+			}
+		}
+
+		private void ResetarSimulacao()
+		{
+			timer1.Stop();
+
+			if (processoSimulacao != null && !processoSimulacao.HasExited)
+			{
+				processoSimulacao.Kill();
+				processoSimulacao.Dispose();
+			}
+
+			caminhoPPM = null;
+			caminhoOUT = null;
+			pastaFrames = null;
+
+			totalFrames = 0;
+			frameIndex = 0;
+
+			txtPPM.Clear();
+			txtSIM.Clear();
+			picTELA.Image = null;
+
+			pBarFrame.Value = 0;
+			pBarFrame.Visible = false;
+
+			simulacaoCarregada = false;
+			btnCarregar.Text = "CARREGAR";
+
+			WriteLineThreadLOG(txtLOG, "Simulação resetada.");
+		}
+	}
+}
